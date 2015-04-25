1d813c2e9f75a32780b813338ab3969e89ae8ae5
Tim Swiggers <tim.swiggers@hotmail.com>
2015-04-25 01:05:52 +0100

    Set the default route to pil, removed the home route

--- src/app.js
+++ src/app.js
@@ -11,11 +11,13 @@ export class App {
     this.router.configure(config => {
       config.title = 'Product Information Label';
       config.map([
-        { route: ['', 'home'], moduleId: 'home'},
+        { route: '', redirect: 'pil' },
         { route: 'pil', moduleId: 'pil/pil-label', title: 'PIL', nav: true },
         { route: 'asl', moduleId: 'asl/asl-label', title: 'ASL', nav: true },
         { route: 'products', moduleId: 'products/products-list', title: 'Products', nav: true },
       ]);
     });
+
+    this.router.navigate('pil');
   }
 }
